25e1763a8f09aa2d5c4ffa6d16506ac64ea0f1f4
Kuba <kubakrzem17@gmail.com>
2020-12-02 22:21:05 +0100

    Added finish messages

--- app/Game.js
+++ app/Game.js
@@ -1,13 +1,15 @@
 import {Deck} from './Deck.js';
+import { Message } from './Message.js';
 import {Player} from './Player.js';
 import { Table } from './Table.js';
 
 class Game
 {
-    constructor({player, table, hitButton, standButton, dealerPoints, playerPoints})
+    constructor({player, table, hitButton, standButton, dealerPoints, playerPoints, messageBox})
     {
         this.hitButton = hitButton;
         this.standButton = standButton;
+        this.messageBox = messageBox;
         this.dealerPoints = dealerPoints;
         this.playerPoints = playerPoints;
         this.player = player;
@@ -58,11 +60,47 @@ class Game
             this.table.showDealerCards(card);
             this.dealerPoints.innerHTML = this.dealer.calculatePoints();
         }
+
+        this.endGame();
+    }
+
+    endGame()
+    {
+        this.hitButton.removeEventListener('click', (e) => this.hitCard());
+        this.standButton.removeEventListener('click', (e) => this.dealerPlays());
+
+        this.hitButton.style.display = 'none';
+        this.standButton.style.display = 'none';
+
+        if(this.player.points <21 && this.player.points == this.dealer.points)
+        {
+            this.messageBox.setText('Remis').show();
+            return;
+        }
+
+        if(this.player.points > 21)
+        {
+            this.messageBox.setText('Wygrywa Krupier').show();
+            return;
+        }
+
+        if(this.dealer.points > 21)
+        {
+            this.messageBox.setText('Wygrałeś!').show();
+            return;
+        }
+
+        if(this.player.points < this.dealer.points)
+        {
+            this.messageBox.setText('Wygrywa Krupier').show();
+            return;
+        }
     }
 }
    
 const player = new Player();
 const table = new Table(document.getElementById('dealersCards'), document.getElementById('playersCards'));
+const messageBox = new Message(document.getElementById('message'));
 const game = new Game({
     hitButton: document.getElementById('hit'),
     standButton: document.getElementById('stand'),
@@ -70,6 +108,7 @@ const game = new Game({
     playerPoints: document.getElementById('playerPoints'),
     player,
     table,
+    messageBox,
 });
 
 game.run();
